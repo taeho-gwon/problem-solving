30cc156d5d4d3fe249b90221845aa77af7b9c886
Taeho <taehogwon96@gmail.com>
2025-09-25 17:16:54 +0900

    boj 1000

---
+++ baekjoon/1000.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// redef type
+using ll = long long;
+using str = string;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+template <class T>
+using vc = vector<T>;
+
+using vi = vc<int>;
+using vl = vc<ll>;
+using vs = vc<str>;
+using vpi = vc<pi>;
+using vpl = vc<pl>;
+using vvi = vc<vi>;
+using vvl = vc<vl>;
+using vvpi = vc<vpi>;
+using vvpl = vc<vpl>;
+
+template <class T>
+using pq = priority_queue<T>;
+template <class T>
+using pqg = priority_queue<T, vc<T>, greater<T>>;
+
+int main()
+{
+    cin.tie(nullptr);
+    cin.sync_with_stdio(false);
+
+    int a, b;
+    cin >> a >> b;
+    cout << a + b;
+    return 0;
+}
